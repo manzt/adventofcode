098d014fda1f20467a0b9fe8963d1cbdb18f42cb
Trevor Manz <trevor.j.manz@gmail.com>
2024-12-01 15:03:54 +0000

    Let the code breathe

--- 2024/src/day01.rs
+++ 2024/src/day01.rs
@@ -7,6 +7,7 @@ mod tests {
     fn part1() -> Result<()> {
         let mut a: Vec<isize> = vec![];
         let mut b: Vec<isize> = vec![];
+
         for line in std::fs::read_to_string("input/day01.txt")?.lines() {
             let (aitem, bitem) = line
                 .split_once(char::is_whitespace)
@@ -14,11 +15,14 @@ mod tests {
             a.push(aitem.trim().parse()?);
             b.push(bitem.trim().parse()?);
         }
+
         // sort location ids
         a.sort_unstable();
         b.sort_unstable();
+
         // total diff for each location
         let diff: isize = a.iter().zip(b.iter()).map(|(a, b)| (a - b).abs()).sum();
+
         assert_eq!(diff, 1388114);
         Ok(())
     }
@@ -36,7 +40,7 @@ mod tests {
             *b.entry(bitem.trim().parse()?).or_insert(0) += 1;
         }
 
-        let total: isize = a
+        let similarity: isize = a
             .iter()
             .map(|(num, times_in_left)| {
                 let times_in_right = b.get(num).unwrap_or(&0);
@@ -44,7 +48,7 @@ mod tests {
             })
             .sum();
 
-        assert_eq!(total, 23529853);
+        assert_eq!(similarity, 23529853);
         Ok(())
     }
 }
